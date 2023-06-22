f4ec55f231faf708b75a6135db8ff667f82966d5
Lydia Lei <llei194@student.fuhsd.org>
2023-06-21 19:03:41 -0700

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls'); 
 const sectBtn = document.querySelectorAll('.control'); 
 const allSections = document.querySelector('.main-content'); 
-
+//test
 
 function PageTransitions(){
     //Button click active class
